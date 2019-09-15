4f5875fed11f0df52f3a37e868077bd612c2a63b
Hanna Dobrzycka <dobrzyckahanna@gmail.com>
2019-09-15 21:25:03 +0200

    Test comment to check slack setup

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const route = express.Router();
 
+// test comment
 router.get("/", async (req, res) => {
   const user = await User.find().sort("name");
   res.send(user);
