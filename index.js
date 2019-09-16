08ac5041aadd4b11cab6925d9058fa2e12f998c5
Hanna Dobrzycka <dobrzyckahanna@gmail.com>
2019-09-16 15:42:42 +0200

    Work almost done for registering new user

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const config = require("config");
 const express = require("express");
 const user = require("./routes/user");
+const auth = require("./routes/auth");
 const app = express();
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined!");
+  process.exit(1);
+}
 
 app.use(express.json());
 app.use("./api/user", user);
+app.use("./api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ routes/auth.js
@@ -0,0 +1,44 @@
+const config = require("config");
+const _ = require("lodash");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User } = require("./models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const route = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
+
+  //next: write logic to compare repeated password during registering new User
+
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+
+  res.send(token);
+});
+
+function validate(req) {
+  const schema = {
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(8)
+      .max(255)
+      .required()
+  };
+
+  return Joi.validate(req, schema);
+}
+
+module.exports = router;
